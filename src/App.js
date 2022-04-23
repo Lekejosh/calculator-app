ef97308d4e50beaa00ed512373e0f5b722c2db8b
lekejosh <lekejosh6wf@gmail.com>
2022-04-23 18:07:55 +0100

    .

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
         <div className="digits">
           {createDigits()}
           <button onClick={() => updateCalc("0")}>0</button>
-          <button onClick={() => updateCalc("0")}>.</button>
+          <button onClick={() => updateCalc(".")}>.</button>
           <button onClick={calculate}>=</button>
         </div>
       </div>
